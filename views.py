4ca6fe88d43b0c37a0a9b659c9c88b43d3c8cf77
Will Murnane <will.murnane@gmail.com>
2012-09-08 15:23:52 -0400

    Add a parameter, and see how that goes

--- views.py
+++ views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from store.models import FandomHierarchy
 
-def frontpage(request):
-	return render_to_response('index.html', {'nodes': FandomHierarchy.objects.all()})
+def frontpage(request, filter):
+	return render_to_response('index.html', {'filter': filter, 'nodes': FandomHierarchy.objects.all()})
