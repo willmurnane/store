c53f03c738ad6357ccd87a506cbc05bc1e2a8474
Will Murnane <will.murnane@gmail.com>
2012-09-08 15:24:26 -0400

    Add default value for filter so things don't break

--- views.py
+++ views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from store.models import FandomHierarchy
 
-def frontpage(request, filter):
+def frontpage(request, filter=None):
 	return render_to_response('index.html', {'filter': filter, 'nodes': FandomHierarchy.objects.all()})
