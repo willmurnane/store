f4d7f7207cff82c38d6973dbef717bfc50345b32
willm1 <willm1@force.dreamhost.com>
2012-09-08 06:49:35 -0700

    Add __unicode__ methods to model

--- models.py
+++ models.py
@@ -3,6 +3,8 @@ from django.db import models
 class FandomHierarchy(models.Model):
 	name = models.CharField(max_length=100)
 	parent = models.ForeignKey('self')
+	def __unicode__(self):
+		return "Fandom tree node %s" % self.name
 	
 class Image(models.Model):
 	pixel_width = models.IntegerField()
@@ -10,8 +12,11 @@ class Image(models.Model):
 	name = models.CharField(max_length=100)
 	fandoms = models.ManyToManyField(FandomHierarchy)
 	image = models.ImageField(upload_to='source_images')
+	def __unicode__(self):
+		return "Image instance %s (%dx%d)" % (self.name, self.pixel_width, self.pixel_height)
 	
 class Media(models.Model):
+	name = models.CharField(max_length=100)
 	visible_width = models.FloatField()
 	visible_height = models.FloatField()
 	cost_cents = models.IntegerField()
@@ -21,3 +26,5 @@ class Media(models.Model):
 	exterior_height = models.FloatField()
 	exterior_depth = models.FloatField()
 	stock_amount = models.IntegerField()
+	def __unicode__(self):
+		return "Media instance %s, %d in stock" % (self.name, self.stock_amount)
