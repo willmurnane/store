4e0ac68d56f9f7240bc1ccbc46d1dbaf25d1f5f7
Will Murnane <will.murnane@gmail.com>
2012-09-08 19:34:45 -0400

    Count up the number of images under each category

--- views.py
+++ views.py
@@ -4,7 +4,18 @@ from store.models import FandomHierarchy, Image
 from store.itemfilters import ApplyPredicates
 
 def frontpage(request):
-	tree = FandomHierarchy.objects.all()
+	# add_related_count(FandomHierarchy.objects.all(), Image, 'foo', 'sub_images', cumulative=True)
+	tree = FandomHierarchy.objects.extra(select={"sub_images": """
+		SELECT (
+			SELECT COUNT(DISTINCT image_id) from %(join_table)s WHERE fandomhierarchy_id in (
+				SELECT id FROM %(data_table)s m2 where m2.tree_id = %(data_table)s.tree_id
+				                                   and m2.lft between %(data_table)s.lft and %(data_table)s.rght
+			)
+		)
+		""" % {"data_table": "store_fandomhierarchy", "join_table": "store_image_fandoms"}
+		})
+	
+	print tree.query.get_compiler('default').as_sql()[0]
 	return render_to_response('index.html', {'filter': None, 'tree': tree})
 
 def filtered(request, predicate):
