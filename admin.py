b3764413b7af545c152f8715edd718b5484094d3
willm1 <willm1@force.dreamhost.com>
2012-09-08 07:15:44 -0700

    Add mptt library, sql script for easy database access, and update settings to use apps/ directory

---
+++ admin.py
@@ -0,0 +1,6 @@
+from store.models import FandomHierarchy, Image, Media
+from django.contrib import admin
+
+admin.site.register(FandomHierarchy)
+admin.site.register(Image)
+admin.site.register(Media)
---
+++ sql
@@ -0,0 +1,1 @@
+mysql -u django_store -p'insecure!' -h mysql.twoevils.net django_store
