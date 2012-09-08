2fe8eaf6a4574ae700187fac6c4dbf163917e323
willm1 <willm1@force.dreamhost.com>
2012-09-08 06:37:57 -0700

    Add initial stab at model

---
+++ models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class FandomHierarchy(models.Model):
+	name = models.CharField(max_length=100)
+	parent = models.ForeignKey('self')
+	
+class Image(models.Model):
+	pixel_width = models.IntegerField()
+	pixel_height = models.IntegerField()
+	name = models.CharField(max_length=100)
+	fandoms = models.ManyToManyField(FandomHierarchy)
+	image = models.ImageField()
+	
+class Media(models.Model):
+	visible_width = models.FloatField()
+	visible_height = models.FloatField()
+	cost_cents = models.IntegerField()
+	price_cents = models.IntegerField()
+	weight_oz = models.FloatField()
+	exterior_width = models.FloatField()
+	exterior_height = models.FloatField()
+	exterior_depth = models.FloatField()
+	stock_amount = models.IntegerField()
