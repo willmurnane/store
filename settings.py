8824d89b9ca2642be175bb9af2306df606c296d4
Will Murnane <will.murnane@gmail.com>
2012-09-08 21:05:58 -0400

    Set proper path for media_root

--- settings.py
+++ settings.py
@@ -60,7 +60,7 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/willm1/twoevils.net/public/media'
+MEDIA_ROOT = '/home/willm1/store.twoevils.net/public/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
