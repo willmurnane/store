dd2a0d8e87c921e3b04f18a1838c51f5c0e4be3e
Will Murnane <will.murnane@gmail.com>
2012-09-15 16:54:23 -0400

    SQLite and MySQL compatible syntax

--- sql/rotatedmedia.sql
+++ sql/rotatedmedia.sql
@@ -1,15 +1,12 @@
-DROP VIEW IF EXISTS store_RotatedMedia;
-CREATE VIEW store_RotatedMedia AS
-	SELECT id as media_ptr_id, name, cost_cents, price_cents, weight_oz, exterior_width, exterior_height, stock_amount,
-		visible_height as vh,
-		visible_width as vw,
-		'Portrait' as orientation
-		FROM store_Media	UNION ALL
-	SELECT id as media_ptr_id, name, cost_cents, price_cents, weight_oz, exterior_width, exterior_height, stock_amount,
-		store_Media.visible_width as vh,
-		store_Media.visible_height + 1 as vw,
-		'Landscape' as orientation
-	FROM store_Media
-	WHERE rotateable;
+DROP VIEW IF EXISTS store_rotatedmedia;
+CREATE VIEW store_rotatedmedia AS
+SELECT id as media_ptr_id, name, cost_cents, price_cents, weight_oz, exterior_width, exterior_height, stock_amount, visible_height as vh, visible_width as vw, 'Portrait' as orientation FROM store_media
+UNION ALL
+SELECT id as media_ptr_id, name, cost_cents, price_cents, weight_oz, exterior_width, exterior_height, stock_amount,
+store_media.visible_width as vh,
+store_media.visible_height + 1 as vw,
+'Landscape' as orientation
+FROM store_media
+WHERE rotateable = true;
 
-select * from store_RotatedMedia;
+select * from store_rotatedmedia;
