94cf9a66e73c034e8af1654ee7696727832c8454
Will Murnane <will.murnane@gmail.com>
2012-09-15 16:57:11 -0400

    Order items by name

--- sql/rotatedmedia.sql
+++ sql/rotatedmedia.sql
@@ -1,5 +1,6 @@
 DROP VIEW IF EXISTS store_rotatedmedia;
 CREATE VIEW store_rotatedmedia AS
+SELECT * FROM (
 SELECT id as media_ptr_id, name, cost_cents, price_cents, weight_oz, exterior_width, exterior_height, stock_amount, visible_height as vh, visible_width as vw, 'Portrait' as orientation FROM store_media
 UNION ALL
 SELECT id as media_ptr_id, name, cost_cents, price_cents, weight_oz, exterior_width, exterior_height, stock_amount,
@@ -7,6 +8,6 @@ store_media.visible_width as vh,
 store_media.visible_height + 1 as vw,
 'Landscape' as orientation
 FROM store_media
-WHERE rotateable = true;
+WHERE rotateable) ORDER BY name, orientation;
 
 select * from store_rotatedmedia;
