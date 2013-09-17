f57b5a0eb20e446f8cfe61dbef18035e33534e41
Will Murnane <will.murnane@gmail.com>
2013-09-16 23:51:15 -0400

    Turn off DEBUG flag

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ PROJECT_ROOT = os.path.dirname(__file__)
 sys.path.insert(0, PROJECT_ROOT)
 sys.path.insert(0, os.path.join(PROJECT_ROOT, "apps"))
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
